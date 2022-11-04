422f5f260ef5caad8b2ceb294287d18f631cd514
AnkitaYadav2104 <106725592+AnkitaYadav2104@users.noreply.github.com>
2022-11-04 12:29:03 +0530

    initial commit with working configs

---
+++ index.js
@@ -0,0 +1,91 @@
+#!/usr/bin/env node
+import fs from "fs";
+import git from "gift";
+import inquirer from "inquirer";
+import { exec } from "child_process";
+import { repos } from "./repositories.js";
+
+const writeIntoFile = async (path, projectName, branch) => {
+  await new Promise((resolve) =>
+    git.clone(path, projectName, null, branch, (err, _repo) => {
+      if (!err) {
+        resolve(_repo);
+        const fileName = `./${projectName}/package.json`;
+        var src = fs.readFileSync(fileName);
+        var srcObj = JSON.parse(src);
+        srcObj.name = projectName;
+
+        var updatedJson = JSON.stringify(srcObj, null, 4);
+        fs.writeFile(fileName, updatedJson, function (err) {
+          if (err) {
+            return console.error(err);
+          }
+        });
+        var Proc = exec(
+          "npm i",
+          {
+            cwd: `./${projectName}`,
+          },
+          function (error, stdout, stderr) {}
+        );
+        console.log(Proc);
+      } else {
+        console.log(" Not able to authenticate you");
+      }
+    })
+  );
+};
+
+(async function () {
+  const { multipleUser } = await inquirer.prompt([
+    {
+      type: "confirm",
+      name: "multipleUser",
+      message: "Do you have mulitple user setup for your github account?",
+      default: true,
+    },
+  ]);
+  let userName;
+  if (multipleUser) {
+    const { name } = await inquirer.prompt([
+      {
+        type: "text",
+        name: "name",
+        message: "Enter your user Name:",
+      },
+    ]);
+    userName = name;
+  }
+  const { project } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "project",
+      message:
+        "Which Project you want to clone from fiftyfive technology organization?",
+      choices: Object.keys(repos),
+    },
+  ]);
+
+  const { projectName } = await inquirer.prompt([
+    {
+      type: "text",
+      name: "projectName",
+      message: "Enter the name for your Project:",
+    },
+  ]);
+  if (project === "react-javascript") {
+    const { path, name, branch } = repos["react-javascript"];
+    console.log(`Cloning ${name} into your local machine`);
+    if (multipleUser) {
+      path.replace("github.com", userName);
+    }
+    return writeIntoFile(path, projectName, branch);
+  } else {
+    const { path, name, branch } = repos["react-typescript"];
+    console.log(`Cloning ${name} into your local machine`);
+    if (multipleUser) {
+      path.replace("github.com", userName);
+    }
+    return writeIntoFile(path, projectName, branch);
+  }
+})();
---
+++ repositories.js
@@ -0,0 +1,12 @@
+export const repos = {
+  "react-typescript": {
+    name: "React-Typescript",
+    path: "git@github.com:FiftyfiveTech/React_boilerplate_typescript.git",
+    branch: "master",
+  },
+  "react-javascript": {
+    name: "React-Javascript",
+    path: "git@github.com:FiftyfiveTech/React-boilerplate-Javascript.git",
+    branch: "master",
+  },
+};
